55c172e7076902987f15f005be78702d3b934fcc
Florentino Carrera <70342537+TinoCarrera@users.noreply.github.com>
2020-09-18 16:39:32 -0400

    Acessors

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -20,7 +20,7 @@
                 <tbody>
                     @foreach($posts as $post)
                         <tr>
-                            <td>Image</td>
+                            <td><img src="{{ $post->featured }}" alt="{{ $post->title }}" width="60px" height="50px"></td>
                             <td>{{ $post->title }}</td>
                             <td>Edit</td>
                             <td>Delete</td>
